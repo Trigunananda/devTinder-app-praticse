50bd60263ce7a3ad8deae797ba0ba50e615b95b8
Trigunananda <swaint214@gmail.com>
2026-03-26 13:08:04 +0530

    exploring get,post,delete,oredr of the routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 
-// Define specific routes with app.get()
-app.get("/",(req,res)=>{
-    res.send("Welcome to the NodeJs");
+//Specific routes first, dynamic routes later
+// Express checks routes top to bottom
+app.get("/hello/2", (req, res) => {
+  res.send("Hello 2 Page");
+});
+
+app.get("/hello/:id", (req, res) => {
+  res.send("Dynamic Route");
+});
+
+//this will handle only get call to user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"NodeJs",lastName:"Backend"});
 })
-app.get("/hello",(req,res)=>{
-    res.send("Hello From the Server")
+
+app.post("/user",(req,res)=>{
+    //saving data to DB
+    res.send("Data Successfully Saved to the DB");
 })
-app.get("/test",( req,res)=>{
-   res.send("Test the app Thoroughly")
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully");
 })
 
 // Start the server
