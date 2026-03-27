931d3c5049b67f7a99d454eda980b69dd7b33fa0
Trigunananda <swaint214@gmail.com>
2026-03-27 10:27:45 +0530

    middleware chain in Express

--- src/app.js
+++ src/app.js
@@ -1,52 +1,36 @@
 const express = require("express");
 const app = express();
 
-// Express supports dynamic routing using params, query strings for filtering, and
-// pattern-based routing using special characters and regex
+// Middleware runs top → bottom
+// ❌ You cannot use res.send() twice
+// 👉 ❌ You cannot call next() after res.send()
+// What if next() is missing?
+// 👉 ❌ Request will hang
+// 👉 ❌ Next middleware will NOT run
+// Simple Flow Diagram
+// Request → 1 → 2 → 3 → 4 → Response
 
-//2. Special Characters in Routes
-// Optional ?  Old Express (v4):"/ab?c", New Express (v5):/ab?c/  Matches: - /ac,/abc
-app.get(/ab?c/, (req, res) => {
-    res.send({ firstName: "ab", lastName: "?c" });
-});
-
-// + → repeat means One or more, Matches: - /abc,/abbc,/abbbc
-app.get(/ab+c/, (req,res)=>{
-    res.send({ firstName: "ab", lastName: "+c" });
-});
-
-// * → Anything,  Matches: - /abcd,/abxyzcd,/ab123cd
-app.get(/ab*cd/,(req,res)=>{
-    res,send({ firstName: "ab",lastName: "*cd"})
-})
-
-// () → Grouping, Matches: - /abe,/abcde 
-app.get(/ab(cd)?e/,(req,res)=>{
-    res.send({ firstName: "ab",lastName: "*cd"})
-})
-
-// 3. Regex Routes
-// Matches any route containing a,  Matches: - /apple,/cat,/bat
-app.get(/a/, (req, res) => {
-    res.send({firstName:"Nate",lastName:"Diaz"})
-})
-
-// $ = ends with,.* = anything before , Matches: - /butterfly ,/dragonfly 
-app.get(/.*fly$/, (req, res) => {
-    res.send({firstName:"Lebron",lastName:"James"})
-})
-
-// 4. Query Params, Matches: - /user?name=tn&age=25
-app.get("/user", (req, res) => {
-    console.log(req.query);
-    res.send({ firstName: "Express", lastName: "Js" });
-});
-// 5. Dynamic Routes, Matches: - user/07/swain/swain@123
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({ firstName: "tn", lastName: "swain" });
-});
-
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user 1");
+    // res.send("Response1!!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user 2");
+    // res.send("Response2!!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user 3");
+    // res.send("Response3!!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user 4");
+    // 👉 Ends the request
+    res.send("Response4!!!");
+},
+)
 
 app.listen(6666, () => {
     console.log("Server Listening on Port 6666");
