46b5ce410baa5dbaee4d731cb2c4b21f66c63ebc
Trigunananda <83834364+Trigunananda@users.noreply.github.com>
2026-03-27 15:11:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const app = express();
 // Middleware runs top → bottom
 // ❌ You cannot use res.send() twice
 // 👉 ❌ You cannot call next() after res.send()
+//  next() → allows request
+// res.send() → stops request
+
 // What if next() is missing?
 // 👉 ❌ Request will hang
 // 👉 ❌ Next middleware will NOT run
 // Simple Flow Diagram
 // Request → 1 → 2 → 3 → 4 → Response
+
 
 app.use("/user",(req,res,next)=>{
     console.log("Handling the route user 1");
